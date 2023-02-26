c325fd3b27a419d29a1bf348210a3626500dd419
Katerina <120592766+muzkatttt@users.noreply.github.com>
2023-02-26 13:45:34 +0300

    Update MyProject.cs

--- MyProject.cs
+++ MyProject.cs
@@ -9,3 +9,37 @@
 [“hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → [] */
+
+void Main()
+{
+    Console.Write($"Введите количество строковых элементов: ");
+    int elem = int.Parse(Console.ReadLine()!);
+    string[] array = new string[elem];
+    int j = 0;
+
+    for (int i = 0; i < elem; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент: ");
+        string element = Convert.ToString(Console.ReadLine()!);
+
+        if (element.Length < 4)
+        {
+            array[j] = element;
+            j++;
+        }
+    }
+    PrintArray(array);
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Main();
+
